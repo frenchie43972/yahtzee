6ca4403ef61128dc30d3ad6c484b8b19347490d6
Kris French <frenchie43972@gmail.com>
2025-04-15 18:31:48 +0900

    Working the score card grid

--- js/game-board.js
+++ js/game-board.js
@@ -1,9 +1,33 @@
-const scoreCardContainer = document.querySelector('score-card-container');
+const scoreCardContainer = document.querySelector('.score-card-container');
 
 function createTitleColumns() {
-  for (let i = 0; i < 7; i++) {
-    scoreCardContainer.textContent += `<div class='score-column>Hello</div>'`;
+  const columnTitles = ['Category', 'How To Score', 'Score'];
+
+  // Iterates six times to create as many div elements and assigns class score-column
+  for (let i = 0; i < 6; i++) {
+    const columnTitleDiv = document.createElement('div');
+
+    // Use modulo to cycle through columnTitles. When i exceeds the number of titles,
+    // i % columnTitles.length resets to 0, effectively repeating the titles.
+    const title = columnTitles[i % columnTitles.length];
+
+    columnTitleDiv.classList.add('score-column');
+
+    // Assigns the text to each dive from the title variable
+    // ensuring all colums get the appropriate title
+    columnTitleDiv.textContent = title.toLocaleUpperCase();
+    scoreCardContainer.appendChild(columnTitleDiv);
   }
 }
 
+function createRows() {
+  for (let i = 0; i < 7; i++) {
+    const rowDiv = document.createElement('div');
+    rowDiv.classList.add('row-items');
+
+    scoreCardContainer.appendChild(rowDiv);
+  }
+}
+
+createRows();
 createTitleColumns();
