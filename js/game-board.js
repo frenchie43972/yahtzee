5d8fce4a738707c720b1c53e1de1980216626fda
Kris French <frenchie43972@gmail.com>
2025-04-16 18:14:16 +0900

    Working the score card

--- js/game-board.js
+++ js/game-board.js
@@ -1,33 +1,23 @@
-const scoreCardContainer = document.querySelector('.score-card-container');
+const upperSection = document.querySelector('.upper-section');
+const lowerSection = document.querySelector('.lower-section');
 
-function createTitleColumns() {
-  const columnTitles = ['Category', 'How To Score', 'Score'];
+function createUpperSection() {
+  for (let i = 0; i < 27; i++) {
+    const upperSectionFields = document.createElement('div');
+    upperSectionFields.classList.add('score-card');
 
-  // Iterates six times to create as many div elements and assigns class score-column
-  for (let i = 0; i < 6; i++) {
-    const columnTitleDiv = document.createElement('div');
-
-    // Use modulo to cycle through columnTitles. When i exceeds the number of titles,
-    // i % columnTitles.length resets to 0, effectively repeating the titles.
-    const title = columnTitles[i % columnTitles.length];
-
-    columnTitleDiv.classList.add('score-column');
-
-    // Assigns the text to each dive from the title variable
-    // ensuring all colums get the appropriate title
-    columnTitleDiv.textContent = title.toLocaleUpperCase();
-    scoreCardContainer.appendChild(columnTitleDiv);
+    upperSection.appendChild(upperSectionFields);
   }
 }
 
-function createRows() {
-  for (let i = 0; i < 7; i++) {
-    const rowDiv = document.createElement('div');
-    rowDiv.classList.add('row-items');
+function createLowerSection() {
+  for (let i = 0; i < 27; i++) {
+    const lowerSectionFields = document.createElement('div');
+    lowerSectionFields.classList.add('score-card');
 
-    scoreCardContainer.appendChild(rowDiv);
+    lowerSection.appendChild(lowerSectionFields);
   }
 }
 
-createRows();
-createTitleColumns();
+createUpperSection();
+createLowerSection();
