ae1acadf8446c9f21f0c23f007d480e42aa06a39
Kris French <frenchie43972@gmail.com>
2025-04-18 17:38:50 +0900

    Refactoring moadl and landing

--- js/game-board.js
+++ js/game-board.js
@@ -1,11 +1,15 @@
 const upperSection = document.querySelector('.upper-section');
 const lowerSection = document.querySelector('.lower-section');
 
+let categoryTitles = [''];
+const upperHeaders = ['Upper Section', 'How To Score', 'Score'];
+const lowerHeaders = ['Lower Section', 'How To Score', 'Score'];
+
 function createUpperSection() {
   for (let i = 0; i < 27; i++) {
     const upperSectionFields = document.createElement('div');
-    upperSectionFields.classList.add('score-card');
 
+    upperSectionFields.classList.add('score-card');
     upperSection.appendChild(upperSectionFields);
   }
 }
