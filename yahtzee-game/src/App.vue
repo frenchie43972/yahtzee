2a7f0e64eda2ffaa2dbfa11f70d934ee2c290397
Kris French <frenchie43972@gmail.com>
2025-12-22 17:23:41 +0900

    Initial scaffold and not much progress

--- yahtzee-game/src/App.vue
+++ yahtzee-game/src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <h1>Welcome, Everything is Fine!</h1>
+  <GameBoard />
 </template>
 
-<script setup></script>
+<script setup>
+import GameBoard from './views/GameBoard.vue'
+</script>
 
 <style scoped></style>
---
+++ yahtzee-game/src/components/BaseButton.vue
@@ -0,0 +1,15 @@
+<template>
+  <button @click="requestRoll">
+    <slot />
+  </button>
+</template>
+
+<script setup>
+const emit = defineEmits(['rollRequest'])
+
+function requestRoll() {
+  emit('rollRequest')
+}
+</script>
+
+<style></style>
